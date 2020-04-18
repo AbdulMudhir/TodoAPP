232cd1bc33c515556d31688850bb04ac934b8378
abdul mudhir <abdulmudhirss@gmail.com>
2020-04-18 22:49:29 +0100

    heroku settings

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class ToDoModel(models.Model):
     username = models.ForeignKey(User, on_delete=models.CASCADE, default=User)
     title = models.CharField(max_length=100, default='')
     content = models.TextField( default='')
-    date_created = models.DateTimeField(default=datetime.today())
+    date_created = models.DateTimeField(default=datetime.today)
     target_date = models.DateTimeField(default='')
     mark = models.BooleanField(default=False)
 
