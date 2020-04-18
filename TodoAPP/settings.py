a3d6e86eaac990c01f24a524a6046a8a2c6cd4d8
abdul mudhir <abdulmudhirss@gmail.com>
2020-04-18 21:09:11 +0100

    heroku settings

--- TodoAPP/settings.py
+++ TodoAPP/settings.py
@@ -127,14 +127,19 @@ USE_L10N = True
 USE_TZ = True
 
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 # Extra places for collect static to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 django_heroku.settings(locals())
