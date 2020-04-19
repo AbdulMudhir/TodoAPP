591e06ce0bace8f2db96ede88b7b9723d0dddb3b
abdul mudhir <abdulmudhirss@gmail.com>
2020-04-19 23:30:03 +0100

    disabled debug mode

--- TodoAPP/settings.py
+++ TodoAPP/settings.py
@@ -34,7 +34,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = os.environ.get('secretkey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["todolistprojectapp.herokuapp.com"]
 
